eef72c9733a1eb5d415967eb17e94730ead72d22
1ska2suwkd <mickey050110@naver.com>
2025-08-31 12:39:48 +0000

    assets Image , Sound 폴더 추가

--- js/scenes/PreloadScene.js
+++ js/scenes/PreloadScene.js
@@ -4,10 +4,11 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('customCursor', 'assets/UI/Pointers/cursor.png');
-        this.load.image('enemy', 'assets/Deco/scarecrow.png')
+        this.load.image('customCursor', 'assets/Image/UI/Pointers/cursor.png');
+        this.load.image('enemy', 'assets/Image/Deco/scarecrow.png')
+        this.load.audio('')
 
-        this.load.spritesheet('warrior', 'assets/Factions/Knights/Troops/Warrior/Blue/Warrior_Blue.png', {
+        this.load.spritesheet('warrior', 'assets/Image/Factions/Knights/Troops/Warrior/Blue/Warrior_Blue.png', {
             frameWidth: 192,
             frameHeight: 192
         });
--- js/systems/Player/PlayerInput.js
+++ js/systems/Player/PlayerInput.js
@@ -1,6 +1,5 @@
 // js/systems/player/PlayerInput.js
 export function setupPlayerInput(ctrl, scene) {
-    // ctrl.cursors = scene.input.keyboard.createCursorKeys();
     ctrl.keys = scene.input.keyboard.addKeys({
         up: Phaser.Input.Keyboard.KeyCodes.W,
         left: Phaser.Input.Keyboard.KeyCodes.A,
